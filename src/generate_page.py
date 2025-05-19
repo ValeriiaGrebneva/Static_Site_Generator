f91b8ece8b322da6b836880fc6dbd29cbdbb0ee3
ValeriiaGrebneva <grebneva.lera97@gmail.com>
2025-05-19 15:04:07 -0700

    Final preparations for the website

--- src/generate_page.py
+++ src/generate_page.py
@@ -7,7 +7,7 @@ def extract_title(markdown):
         raise Exception("no header (# ) found")
     return markdown[2:].split("\n")[0].strip(" ")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     try:
@@ -30,7 +30,7 @@ def generate_page(from_path, template_path, dest_path):
     html_string = markdown_to_html_node(content_from_path)
     html_string = html_string.to_html()
   
-    template = content_template_path.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    template = content_template_path.replace("{{ Title }}", title).replace("{{ Content }}", html_string).replace('href="/', f'href="{basepath}').replace('src="', f'src="{basepath}')
 
     os.makedirs("/".join(dest_path.split("/")[:-1]), mode=0o777, exist_ok=True)
     
@@ -38,12 +38,12 @@ def generate_page(from_path, template_path, dest_path):
         file_dest_path.write(template)
     
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     files=os.listdir(dir_path_content)
     for fname in files:
         if os.path.isfile(dir_path_content + "/" + fname):
-            generate_page(os.path.join(dir_path_content, fname), template_path, os.path.join(dest_dir_path, fname[:-3] + ".html"))
+            generate_page(os.path.join(dir_path_content, fname), template_path, os.path.join(dest_dir_path, fname[:-3] + ".html"), basepath)
         else:
             os.makedirs(dest_dir_path + "/" + fname, mode=0o777, exist_ok=True)
-            generate_pages_recursive(dir_path_content + "/" + fname, template_path, dest_dir_path + "/" + fname)
+            generate_pages_recursive(dir_path_content + "/" + fname, template_path, dest_dir_path + "/" + fname, basepath)
     
--- src/main.py
+++ src/main.py
@@ -1,28 +1,31 @@
 from pathlib import Path
-import shutil, os
+import shutil, os, sys
 from generate_page import generate_pages_recursive
 
 def recursive_call(path_to_files):
-    if path_to_files == "" and "public" not in os.listdir():
-        os.mkdir('public')
+    basepath = ""
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
-    for path in Path("public" + "/" + path_to_files).glob("**/*"):
+    os.makedirs("docs", mode=0o777, exist_ok=True)
+
+    for path in Path("docs" + "/" + path_to_files).glob("**/*"):
         if path.is_file():
             path.unlink()
         elif path.is_dir():
             shutil.rmtree(path)
     
     source = 'static' + "/" + path_to_files
-    destination = 'public' + "/" + path_to_files
+    destination = 'docs' + "/" + path_to_files
     files=os.listdir(source)
     for fname in files:
         if os.path.isfile(source + "/" + fname):
             shutil.copy(os.path.join(source,fname), destination)
         else:
-            os.mkdir('public' + "/" + path_to_files + "/" + fname)
+            os.mkdir('docs' + "/" + path_to_files + "/" + fname)
             recursive_call(path_to_files + "/" + fname)
 
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 def main():
